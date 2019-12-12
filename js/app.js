301dacaf000d3088f3b85c0fd4c5a3bd7a8aeb04
ecuber <37519236+ecuber@users.noreply.github.com>
2019-12-12 12:47:49 -0500

    Added types. Should be compatible with frontend search box
    needs to be tested

--- js/app.js
+++ js/app.js
@@ -4,38 +4,41 @@
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
  */
 
-function extractTypes(typeList) {
+/**
+ * Extracts the type/ability names from an array in the original format from the JSON object.
+ * @param attribute - "ability" or "type" to be extracted
+ * @param arr - Original array
+ * @returns {[]}
+ */
+function extract(attribute, arr) {
     let newList = [];
-    for (let i = 0; i < typeList.length; i++) {
-        newList.push(typeList[i].type.name);
+    for (let i = 0; i < arr.length; i++) {
+        newList.push(arr[i][attribute].name);
     }
     return newList;
 }
 
 /**
- * Returns an object with the attriutes of the desired Pokemon.
- * @param pokemon - String, the name or ID of the Pokemon.
+ * Returns an object with the attributes of the Pokemon entered in the search box.
  */
-function getPokemon(pokemon) {
+function getPokemon() {
+    const pokemon = $("#searchBox").text();
     $.get(`https://pokeapi.co/api/v2/pokemon/${pokemon}/`, (data, status) => {
-        console.log(status);
+        // console.log(status);
         if (status === "success") {
-            console.log(data.name);
+            // console.log(data.name);
             return {
                 name: data.name,
-                types: extractTypes(data.types),
+                types: extract("type", data.types),
+                abilities: extract("ability", data.abilities),
                 sprites: data.sprites
             };
         } else {
             throw newError(pokemon);
         }
-    }).then(r => {
-        // TODO: figure out what to do with this promise-- anything?
     });
 }
 
 function newError(id) {
     return new Error(`No Pokemon with name/ID ${id} found.`)
 }
-
-// console.log(getPokemon("bulbasaur"));

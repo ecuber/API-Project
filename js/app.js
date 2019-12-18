ea9a41548167c19001e602184874729f46e32e6b
ecuber <37519236+ecuber@users.noreply.github.com>
2019-12-18 12:40:01 -0500

    Added smooth scrolling

--- js/app.js
+++ js/app.js
@@ -30,7 +30,7 @@ function displayPokemon() {
     $('#pokemon-title').html('<h1 class ="pokemon-name">' +
         currentPokemon.name.charAt(0).toUpperCase() + currentPokemon.name.substring(1)
         + '</h1> ' +
-        '<img class="pixel-art" src="'+currentPokemon.sprites.front_default+'">');
+        '<img class="pixel-art" src="' + currentPokemon.sprites.front_default + '">');
     // console.log(currentPokemon);
     $('#pokemon-display').html(
         `<h1 class ="pokemon-name">${currentPokemon.name.charAt(0).toUpperCase()}${currentPokemon.name.substring(1)}</h1>
@@ -38,7 +38,7 @@ function displayPokemon() {
             <img class="sprite" src="${currentPokemon.sprites.front_default}" height="300px">
             <ul class="pokemon-stats">
                 <li class="pokemon-stats">Types: ${currentPokemon.types.join(", ")}</li>
-                <li>Abilities: ${currentPokemon.abilities.join(", ")}</li>
+                <li class="pokemon-stats">Abilities: ${currentPokemon.abilities.join(", ")}</li>
             </ul>
         </div>
         `);
@@ -65,6 +65,13 @@ function loadPokemon() {
             };
             currentPokemon = obj;
             displayPokemon();
+
+            $("#header")[0].scrollIntoView({
+                behavior: "smooth",
+                block: "start",
+                inline: "nearest"
+
+            });
         }
     }).fail(() => {
         oopsies(pokemon);
